73d35fad304d18542f1534255ee86ab2a612ab0a
Matias Gea <matigea@gmail.com>
2016-09-01 13:00:02 -0300

    Modified oled interface

--- user_interfaces/oled_128x64/oled_interface.py
+++ user_interfaces/oled_128x64/oled_interface.py
@@ -15,7 +15,7 @@ class output_interface():
 
         self.__device = ssd1306(port=1, address=0x3C)
         self.__dirname = os.path.dirname(os.path.realpath(__file__))
-        self.__font = ImageFont.truetype(self.__dirname + '/fonts/onesize.ttf', 15)
+        self.__font = ImageFont.truetype(self.__dirname + '/fonts/onesize.ttf', 14)
 
         self.custom_glyphs = custom_glyphs.get_data()
 
@@ -29,24 +29,31 @@ class output_interface():
 
         ## Update LCD
         humidity  = "{:.1f}%".format(data['humidity'])
-        temperature = "{:.1f}'".format(data['temp'])
-        time = '{time}'
+        temperature = "{:.1f}".format(data['temp'])
+        time = strftime("%H:%M", localtime())
         desired = "Target: "
         if data['standby']:
-             desired = desired + "OFF"
+            desired = desired + "OFF"
         else:
-             desired = desired + "{:.1f}'".format(data['desired'])
+            desired = desired + "{:.1f}".format(data['desired'])
 
-        time = strftime("%H:%M", localtime())
         with canvas(self.__device) as draw:
             draw.bitmap((0, 0), self.get_glyph('hum'), fill=255)
             draw.text((12, 0), humidity, font=self.__font, fill=255)
             draw.bitmap((64, 0), self.get_glyph('temp'), fill=255)
             draw.text((76, 0), temperature, font=self.__font, fill=255) 
+            symbolpos = draw.textsize(temperature, font=self.__font)
+            draw.bitmap((76+symbolpos[0],0), self.get_glyph('degrees'), fill=255)
 
             draw.text((0, 32), desired, font=self.__font, fill=255) 
-            draw.bitmap((0, 48), self.get_glyph('time'), fill=255)
-            draw.text((12, 48), time, font=self.__font, fill=255) 
+            if not data['standby']:
+                symbolpos = draw.textsize(desired, font=self.__font)
+                draw.bitmap((symbolpos[0],32), self.get_glyph('degrees'), fill=255)
+
+            textsize = draw.textsize(time, font=self.__font)
+            textpos = 128 - textsize[0]
+            draw.bitmap((textpos - 10, 48), self.get_glyph('time'), fill=255)
+            draw.text((textpos, 48), time, font=self.__font, fill=255) 
 
     def turn_off(self):
         #self.__device.command(const.DISPLAYOFF)
@@ -57,8 +64,6 @@ class output_interface():
             draw.bitmap((0, 0), logo, fill=1)
 
     def turn_on(self):
-        with canvas(self.__device) as draw:
-            draw.rectangle((0, 0, self.__device.width, self.__device.height), border=0, fill=0)
         if self.__saved_data:
             self.render(self.__saved_data)
         #self.__device.command(const.DISPLAYON)
